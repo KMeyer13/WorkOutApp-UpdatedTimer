e01c7140d43120dbc5437b7fe725c396d6eac2a0
KMeyer13 <meyerrkyle@aol.com>
2024-02-22 14:44:45 -0600

    update to vite config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/WorkOutApp-UpdatedTimer/",
+  // base: "/WorkOutApp-UpdatedTimer/",
   plugins: [react()],
   root: "src",
 });
